2e7cd16c7be54358a569de1eb6c343990de4c7de
Luca Franchini Queiroz <luca-queiroz@univem.edu.br>
2021-04-22 04:27:45 -0300

    Perfil

---
+++ LoCar/Pages/Perfil/Perfil.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoCar.Pages
+{
+    public class PerfilModel : PageModel
+    {
+        private readonly ILogger<PerfilModel> _logger;
+
+        public PerfilModel(ILogger<PerfilModel> logger)
+        {
+           _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
